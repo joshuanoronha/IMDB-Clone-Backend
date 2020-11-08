8a7a3e1822a929f811f67baf970ea5e256af31be
Joshua Noronha <noronhajoshua1@gmail.com>
2020-11-09 01:31:06 +0530

    Fixed bugs in deploy

---
+++ config/secrets/connection.js
@@ -0,0 +1,4 @@
+module.exports = {
+  database: 'mongodb+srv://admin:jenna258456@cluster0.i5nbs.mongodb.net/imdbClone?retryWrites=true&w=majority',
+  jwt: 'thisisareallyhardstringtoguesssoyoucannothackitunderstood?',
+};
